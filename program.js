a65410fd62ad9c649dfe0b392f612c917f9b1c3b
BBernYY <66414852+BBernYY@users.noreply.github.com>
2024-01-17 10:40:53 +0100

    initial commit

--- program.js
+++ program.js
@@ -1,3 +1,17 @@
-function test(){
-    alert('true');
+function interperet(str){
+    const reg1 = /(^|\n)\d+\. /g
+    const reg2 = /\n(\D.*)/g
+    const reg3 = /: /g
+    const reg4 = /-\n/g
+    str = str.replace(reg4, '')
+    str = str.replace(reg3, '=')
+    str = str.replace(reg2, '$1')
+    str = str.replace(reg1, '$1')
+    return str
+}
+function wrap(){
+    var i = document.getElementById('in')
+    i.value = interperet(i.value)
+
+    
 }
